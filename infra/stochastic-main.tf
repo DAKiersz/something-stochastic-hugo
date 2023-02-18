3f180bc586f3733def400c84dc52cc8a3f9bf8f9
DAKiersz <59928295+DAKiersz@users.noreply.github.com>
2023-02-18 21:15:39 +0000

    Add features, declare provider

--- infra/stochastic-main.tf
+++ infra/stochastic-main.tf
@@ -12,9 +12,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "=3.0.1"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
 
 # Resource Group
