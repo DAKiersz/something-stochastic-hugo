6ef7327c688287bb75d3b0326d3fc7ce8c474e64
Dominik Kiersz <59928295+DAKiersz@users.noreply.github.com>
2023-02-19 11:40:55 +0000

    Website infrastructure (#3)
    * Infra WIP
    
    * WIP - infra pipeline
    
    * Infra
    
    * update stochastic.main.tf
    
    * Add features, declare provider
    
    * More steps to stochastic-infra.yml
    
    * Change to westeu
    
    * change name of statis site
    
    * New provider
    
    * stochastic-cicd upload
    
    * build only
    
    * Try to run on PR
    
    * Remove dynamic version checking
    
    * change path
    
    * Use artifacts
    
    * path test
    
    * change task versions
    
    * try public
    
    * Correct artifacts
    
    * Use home paths
    
    * More specific path
    
    * Try with repo
    
    * Delete API
    
    * WIP
    
    * change CICD
    
    * Remove baseURL for now

---
+++ infra/stochastic-main.tf
@@ -0,0 +1,46 @@
+locals {
+  name        = "somethingstochastic"
+  location    = "westeurope"
+  environment = "prod"
+  common_tags = {
+    project     = "somethingstochastic"
+    environment = "prod"
+  }
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.1"
+    }
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
+}
+
+provider "azapi" {
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${local.name}-${local.environment}-${local.location}"
+  location = local.location
+  tags     = local.common_tags
+}
+
+# Azure Static Web App
+resource "azurerm_static_site" "staticsite" {
+  name                = "swp-${local.name}-${local.environment}-${local.location}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tags                = local.common_tags
+  sku_size            = "Free"
+  sku_tier            = "Free"
+}
